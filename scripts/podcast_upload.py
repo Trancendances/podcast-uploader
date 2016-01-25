8458802ea9d5be412f97d0ce926afebb20b39e52
Brendan Abolivier <contact@brendanabolivier.com>
2016-01-25 18:23:57 +0100

    Cleaned comments, added error handling

--- scripts/podcast_upload.py
+++ scripts/podcast_upload.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python2
 
 import swiftclient
-import os, sys
+import os, sys, mimetypes
 import requests
 import json
 import pprint
@@ -9,40 +9,48 @@ import pprint
 data = file("../tokens.json")
 tokens = json.load(data)
 
+if len(sys.argv) != 2:
+    print "Usage: podcast_upload.py [audio file name]"
+    exit(1)
+
+# Fetching infos on the file path
 file_path = os.path.abspath(sys.argv[1])
 file_name = os.path.basename(sys.argv[1])
 
-# Fetching infos on the filename
-episode = file(file_path);
-episode_size = os.stat(file_path).st_size
-episode_content = episode.read(episode_size)
+# Opening file
+try:
+    episode = open(file_path)
+except IOError:
+    print "File " + file_path + " not found."
+    exit(1)
 
 # Uploading to Mixcloud
 print "Uploading of " + file_name + " on Mixcloud started...";
-
-files = {"mp3": open(file_path)}
+# Filling the requests parameters
+files = {"mp3": episode}
 url = "https://api.mixcloud.com/upload/"
 params = {"access_token": tokens["mixcloud"]["test_token"]}
 data = {"name": "Test API"}
-
+# API request
 r = requests.post(url, data=data, params=params, files=files)
-
+# Error handling
 if (r.status_code == 200):
     print "Upload to Mixcloud succeeded!"
 else:
     print "Upload to Mixcloud failed with error code " + str(r.status_code) + " (" + r.reason + ")"
     exit(1)
 
-# Connection
-
-# Additional options
+# OpenStack
+# Setting options
 options = {}
 options['tenant_id'] = tokens["openstack"]["tenant_id"]
 options['region_name'] = tokens["openstack"]["region_name"]
-
+# Opening connection
 client = swiftclient.client.Connection(tokens["openstack"]["auth_url"], tokens["openstack"]["username"], tokens["openstack"]["password"], 5, None, None, False, 1, 64, tokens["openstack"]["tenant_name"], options, '2')
-
-# Uploading file
+# Getting infos on the file
+episode_size = os.stat(file_path).st_size
+episode_content = episode.read(episode_size)
+# Uploading
 print "Uploading of " + file_name + " on OpenStack started..."
 try:
     client.put_object("podcasts", file_name, episode_content, episode_size, None, None, "audio/mpeg")
